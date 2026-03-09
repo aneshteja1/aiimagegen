c0226f40bf462df4410dafe922884b48e5f59b6e
aneshteja1 <aneshteja1@gmail.com>
2026-03-09 07:55:35 +0530

    Update companyStore.js

--- frontend/src/store/companyStore.js
+++ frontend/src/store/companyStore.js
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { MOCK_COMPANIES } from '../services/mock/companies.mock.js';
+import { MOCK_COMPANIES } from '../services/mock/companies.mock.js'; 
 
 const useCompanyStore = create((set, get) => ({
   companies: MOCK_COMPANIES,
